e40cb08154608997801c910f3ec0a2a77460cdb6
AlexandreMaquet <alexandre.maquet@rpn.ch>
2017-12-12 13:25:00 +0100

    Création des variables

--- Programme_chiffrage/Program.cs
+++ Programme_chiffrage/Program.cs
@@ -27,12 +27,53 @@ namespace Programme_chiffrage
 
         static void chiffrer()
         {
+            string strMessage;
+            string strCle;
+            int iChiffreSol;
+            
 
+            Console.WriteLine("Entrez votre message à chiffrer : ");
+            strMessage = Console.ReadLine();
+
+            Console.WriteLine("Entrez votre clé de cryptage : ");
+            strCle = Console.ReadLine();
+            int iCleSol = ConvertionCle(strCle);
+
+            iChiffreSol = iMessageSol * iCleSol;
+            Console.WriteLine("Voici votre message codé : {0} ", iChiffreSol);
+            Console.ReadKey();
         }
 
         static void dechiffrer()
         {
 
         }
+
+        static int ConvertionLC(string strMessage)
+        {
+            return 0;
+        }
+
+        static int ConvertionCle(string strCle)
+        {
+            int iPosChar = 0;
+            int iLettre1 = 0;
+            int iLettre2 = 0;
+            int iLettre3 = 0;
+            int iValeurNombre = 0;
+
+            do
+            {
+                 iValeurNombre = strCle.ToCharArray(iPosChar, 1);
+            }
+            while (iPosChar == strCle.Length);
+
+
+
+
+
+            return (0);
+        }
+ 
     }
 }
