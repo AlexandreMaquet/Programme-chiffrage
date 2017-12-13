245b9ba76314cd33ded5a006bf4b59a4cb3ccab1
AlexandreMaquet <alexandre.maquet@rpn.ch>
2017-12-13 09:49:15 +0100

    Début de la base de la partie déchiffrer

--- Programme_chiffrage/Program.cs
+++ Programme_chiffrage/Program.cs
@@ -27,24 +27,7 @@ namespace Programme_chiffrage
 
         static void chiffrer()
         {
-            string strMessage;
-            string strCle;
-            long lChiffreSol;
-
-            Console.Clear();
-
-            Console.WriteLine("Entrez votre message à chiffrer : ");
-            strMessage = Console.ReadLine();
-
-            Console.WriteLine("Entrez votre clé de cryptage : ");
-            strCle = Console.ReadLine();
-
-            long lMessageSol=ConvertionLC(strMessage);
-            long lCleSol=ConvertionCle(strCle);
-
-            lChiffreSol = lMessageSol + lCleSol;
-            Console.WriteLine("Voici votre message codé : {0} ", lChiffreSol);
-            Console.ReadKey();
+            
         }
 
         static void dechiffrer()
@@ -54,52 +37,17 @@ namespace Programme_chiffrage
 
         static long ConvertionLC(string strMessage)
         {
-            int iPosChar = 0;
-            long lValeurNombre;
-            long lConvertionMessage;
-            string strValeurNombre;
-            string strConvertionMessage = "";
-            string strTempVal;
-            char cValeurNombre;
-
-            for (iPosChar = 0; iPosChar < strMessage.Length; iPosChar++)
-            {
-                strValeurNombre = strMessage.Substring(iPosChar, 1);
-                cValeurNombre = Convert.ToChar(strValeurNombre);
-                lValeurNombre = Convert.ToInt64(cValeurNombre);
-                strTempVal = Convert.ToString(lValeurNombre);
-                strConvertionMessage = strConvertionMessage + "0" + strTempVal;
-
-            }
-
-           lConvertionMessage = Convert.ToInt64(strConvertionMessage);
-
-            return lConvertionMessage;
+            
         }
 
         static long ConvertionCle(string strCle)
         {
-            int iPosChar = 0;
-            long iValeurNombre;
-            long iConvertionCle;
-            string strValeurNombre;
-            string strConvertionMessage = "";
-            string strTempVal;
-            char cValeurNombre;
 
-            for (iPosChar = 0; iPosChar < strCle.Length; iPosChar++)
-            {
-                strValeurNombre = strCle.Substring(iPosChar, 1);
-                cValeurNombre = Convert.ToChar(strValeurNombre);
-                iValeurNombre = Convert.ToInt64(cValeurNombre);
-                strTempVal = Convert.ToString(iValeurNombre);
-                strConvertionMessage = strConvertionMessage + "0" + strTempVal;
+        }
 
-            }
+        static long ConvertionCL()
+        {
 
-            iConvertionCle = Convert.ToInt64(strConvertionMessage);
-
-            return iConvertionCle;
         }
  
     }
